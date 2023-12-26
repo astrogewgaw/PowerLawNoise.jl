ea8baa598b280c4d8bc33fc3a203b99548b1d193
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-12-26 11:31:19 +0530

    ✅ Add tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
-using Noisy
 using Test
+using FFTW
+using CurveFit
+using PowerLawNoise
 
-@testset "Noisy.jl" begin
-    # Write your tests here.
+@testset "PowerLawNoise.jl" begin
+    β = 2.0
+    ts = PowerLawNoise.noise(β, 0.0, 1000)
+    ν = rfftfreq(size(ts, 1))
+    P = abs.(rfft(ts)) .^ 2
+    _, b = linear_fit(log10.(ν[2:end]), log10.(P[2:end]))
+    @test -b ≈ β atol = 0.1
 end
